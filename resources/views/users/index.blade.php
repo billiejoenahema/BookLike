cf567c0f705d9a391efecae1fc9e5dfeeef9bc4c
billiejoenahema <billiejoenahema@gmail.com>
2020-07-06 22:41:41 +0900

    route parameter key delete

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -19,14 +19,14 @@
                             @endif
                             <div class="d-flex justify-content-end flex-grow-1">
                                 @if (auth()->user()->isFollowing($user->id))
-                                    <form action="{{ route('unfollow', ['id' => $user->id]) }}" method="POST">
+                                    <form action="{{ route('unfollow', $user->id) }}" method="POST">
                                         {{ csrf_field() }}
                                         {{ method_field('DELETE') }}
 
                                         <button type="submit" class="btn btn-danger">フォロー解除</button>
                                     </form>
                                 @else
-                                    <form action="{{ route('follow', ['id' => $user->id]) }}" method="POST">
+                                    <form action="{{ route('follow', $user->id) }}" method="POST">
                                         {{ csrf_field() }}
 
                                         <button type="submit" class="btn btn-primary">フォローする</button>
