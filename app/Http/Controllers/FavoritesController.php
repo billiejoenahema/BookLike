ab1832a676b96d0e5fc1f73474f7328c528d32e6
billiejoenahema <billiejoenahema@gmail.com>
2020-07-13 20:41:49 +0900

    add: favorite action

---
+++ app/Http/Controllers/FavoritesController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Favorite;
+
+class FavoritesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Favorite $favorite)
+    {
+        $user = auth()->user();
+        $review_id = $request->review_id;
+        $is_favorite = $favorite->isFavorite($user->id, $review_id);
+
+        if(!$is_favorite) {
+            $favorite->storeFavorite($user->id, $review_id);
+            return back();
+        }
+        return back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Favorite $favorite)
+    {
+        $user_id = $favorite->user_id;
+        $review_id = $favorite->review_id;
+        $favorite_id = $favorite->id;
+        $is_favorite = $favorite->isFavorite($user_id, $review_id);
+
+        if($is_favorite) {
+            $favorite->destroyFavorite($favorite_id);
+            return back();
+        }
+        return back();
+    }
+}
--- resources/views/reviews/show.blade.php
+++ resources/views/reviews/show.blade.php
@@ -39,8 +39,23 @@
                         <a href="{{ url('reviews/' .$review->id) }}"><i class="far fa-comment fa-fw"></i></a>
                         <p class="mb-0 text-secondary">{{ count($review->comments) }}</p>
                     </div>
+
                     <div class="d-flex align-items-center">
-                        <button type="" class="btn p-0 border-0 text-primary"><i class="far fa-heart fa-fw"></i></button>
+                    @if (!in_array($user->id, array_column($review->favorites->toArray(), 'user_id'), TRUE))
+                            <form method="POST" action="{{ url('favorites/') }}" class="mb-0">
+                                @csrf
+
+                                <input type="hidden" name="review_id" value="{{ $review->id }}">
+                                <button type="submit" class="btn p-0 border-0 text-primary"><i class="far fa-heart fa-fw"></i></button>
+                            </form>
+                        @else
+                            <form method="POST" action="{{ url('favorites/' .array_column($review->favorites->toArray(), 'id', 'user_id')[$user->id]) }}" class="mb-0">
+                                @csrf
+                                @method('DELETE')
+
+                                <button type="submit" class="btn p-0 border-0 text-danger"><i class="fas fa-heart fa-fw"></i></button>
+                            </form>
+                        @endif
                         <p class="mb-0 text-secondary">{{ count($review->favorites) }}</p>
                     </div>
                 </div>
