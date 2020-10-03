69ee4b6c7eec9838994a613cb6acb54b36695c31
billiejoenahema <billiejoenahema@gmail.com>
2020-10-03 13:16:31 +0900

    add: default_user_image to create_users_table

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,8 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
+                <div class="card-header">
+                    {{ __('Verify Your Email Address') }}
+                </div>
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
