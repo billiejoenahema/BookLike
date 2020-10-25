8965847aa03034ac8362096f020db6142666be8d
billiejoenahema <billiejoenahema@gmail.com>
2020-10-25 15:44:04 +0900

    add: switch index toggle users&reviews

--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
-
 @section('content')
+<div class="d-flex flex-row align-items-center" id="index-toggle">
+    <div class="switch-index bg-blogLight text-light text-center">投稿一覧
+    </div>
+    <a href="{{ url('users') }} " class="text-reset">
+        <div class="switch-index bg-blog text-white text-center align-items-center">ユーザー一覧</div>
+    </a>
+</div>
 <div id="reviewIndex"></div>
 @include('components.new_post_button')
 @endsection
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="d-flex flex-row align-items-center" id="index-toggle">
+    <a href="{{ url('reviews') }} " class="text-reset">
+        <div class="switch-index bg-blog text-light text-center">投稿一覧</div>
+    </a>
+    <div class="switch-index bg-blogLight text-white text-center align-items-center">ユーザー一覧</div>
+</div>
 <div id="userIndex"></div>
 @include('components.new_post_button')
 @endsection
