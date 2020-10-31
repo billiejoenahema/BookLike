5b9048622082d695b362b2dee1d52c1245947246
billiejoenahema <billiejoenahema@gmail.com>
2020-10-31 10:31:49 +0900

    change: new post button transform effect

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,7 +50,7 @@
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
                             aria-haspopup="true" aria-expanded="false">
                             <img src="{{ asset('storage/profile_image/'.$login_user->profile_image) }}"
-                                class="rounded-circle shadow-sm img-fluid" width="36" height="36">
+                                class="rounded-circle shadow-sm img-fluid nav-icon" width="36" height="36">
                         </a>
                         <!-- Dropdown Menu -->
                         <div class="dropdown-menu dropdown-menu-right pb-0" aria-labelledby="dropdownMenuLink">
