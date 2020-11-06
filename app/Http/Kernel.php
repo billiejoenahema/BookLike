b15025c55e070c7f5ed6b509d65cf4be6d1adf36
billiejoenahema <billiejoenahema@gmail.com>
2020-11-06 22:11:26 +0900

    add: https redirect commentout

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \App\Http\Middleware\TrimStrings::class,
-        \App\Http\Middleware\ForceHttps::class,
+        // \App\Http\Middleware\ForceHttps::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
