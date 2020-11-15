562931ee9d3cf54fa5fce56981da297f0f8d91df
billiejoenahema <billiejoenahema@gmail.com>
2020-11-15 15:18:46 +0900

    fix: footer menu display

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use App\User;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
