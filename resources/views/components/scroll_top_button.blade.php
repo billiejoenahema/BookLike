7276544ed4c502296064a790a5018410d2bc018f
billiejoenahema <billiejoenahema@gmail.com>
2020-12-01 20:47:04 +0900

    remove: dead code

--- resources/views/components/scroll_top_button.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="text-center text-blog scroll-top d-flex justify-content-center">
-    <a href="#" onclick="scrollTop" class="text-blog mb-0">
-        <i class="fas fa-angle-up h1"></i>
-        <br />
-        <span>Page Top</span>
-    </a>
-</div>
