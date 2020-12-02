0b1de714ba62cacde46abf0c0ecc08b97a38b451
billiejoenahema <billiejoenahema@gmail.com>
2020-12-02 22:09:07 +0900

    implement: current page highlight in mobile footer-menu item

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -11,11 +11,14 @@
 </footer>
 @auth
 <div id="footer-menu" class="d-flex flex-row justify-content-between text-reset text-center bg-bodyColor">
-    <a href="{{ url('reviews') }}" class="text-blog"><i class="fas fa-book-open"></i><br /><span>Reviews</span></a>
-    <a href="{{ url('users') }}" class="text-blog"><i class="fas fa-users"></i><br /><span>Users</span></a>
-    <a href="{{ url('users/'.$login_user->id) }}" class="text-blog"><i
+    <a href="{{ url('reviews') }}" id="reviewsIcon" class="text-blog footerMenuItem"><i
+            class="fas fa-book-open"></i><br /><span>Reviews</span></a>
+    <a href="{{ url('users') }}" id="usersIcon" class="text-blog footerMenuItem"><i
+            class="fas fa-users"></i><br /><span>Users</span></a>
+    <a href="{{ url('users/'.$login_user->id) }}" id="usersShowIcon" class="text-blog footerMenuItem"><i
             class="fas fa-user-cog"></i><br /><span>MyPage</span></a>
-    <a href="{{ url('reviews/create') }}" class="text-blog"><i class="fas fa-pen"></i><br /><span>NewPost</span></a>
+    <a href="{{ url('reviews/create') }}" id="reviewsCreateIcon" class="text-blog footerMenuItem"><i
+            class="fas fa-pen"></i><br /><span>NewPost</span></a>
     <a href="#" onclick="{scrollTop}" class="text-blog"><i class="fas fa-angle-up"></i><br /><span>TOP</span></a>
 </div>
 @endauth
