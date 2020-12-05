1f5ed4c32a231ce4f38a7da58d92a1e1d9446263
billiejoenahema <billiejoenahema@gmail.com>
2020-12-05 18:14:40 +0900

    add: bookshelf in users/show

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,11 +3,15 @@
 @section('content')
 @include('components.index_toggle')
 @include('components.back_button')
+
 @if($user == $login_user)
 @include('components.login_user_profile')
 @else
 @include('components.user_profile')
 @endif
+
+@include('components.bookshelf')
+
 <!-- React-tabs -->
 <div id="userPageTab"></div>
 @include('components.scroll_to_top_button')
