992b27fb9dca75309a0dee3ac12a103a05467100
billiejoenahema <billiejoenahema@gmail.com>
2020-12-11 15:46:30 +0900

    add: check button in user account delete modal

---
+++ public/js/deleteCheck.js
@@ -0,0 +1,14 @@
+function deleteCheck() {
+    'use strict'
+    const deleteCheck = document.getElementById('deleteCheck')
+    const deleteButton = document.getElementById('deleteButton')
+
+    // チェック済みなら削除ボタンを有効化
+    if (deleteCheck.checked) {
+        deleteButton.classList.remove('disabled')
+        deleteButton.disabled = false
+        return
+    }
+    deleteButton.classList.add('disabled')
+    deleteButton.disabled = true
+}
