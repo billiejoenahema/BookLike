ed208cc56e078ba2684afdb57ded8fc80037c893
billiejoenahema <billiejoenahema@gmail.com>
2020-12-13 10:54:36 +0900

    add: disabled to login button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,7 +42,7 @@
             </div>
 
             <div class="form-group row">
-                <button id="login" type="submit" class="btn btn-primary rounded-pill shadow-sm disabled">
+                <button id="login" type="submit" class="btn btn-primary rounded-pill shadow-sm disabled" disabled>
                     {{ __('ログイン') }}
                 </button>
 
