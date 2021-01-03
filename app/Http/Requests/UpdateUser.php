381076ec08a91618b93538f8a7ea5e8b2391936f
billiejoenahema <billiejoenahema@gmail.com>
2021-01-03 14:01:46 +0900

    refactoring: validate with FormRequest in users/edit

---
+++ app/Http/Requests/UpdateUser.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use App\Models\User;
+
+class UpdateUser extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(User $user)
+    {
+        return [
+            'screen_name'   => ['required', 'string', 'max:50', Rule::unique('users')->ignore($this->user)],
+            'name'          => ['nullable', 'string', 'max:50'],
+            'profile_image' => ['file', 'image', 'mimes:jpeg,png,jpg', 'max:2048'],
+            'category'      => ['string', 'max:255'],
+            'asin'          => ['string', 'max:10'],
+            'story'         => ['string', 'max:800'],
+            'description'   => ['string', 'max:255'],
+            'email'         => ['required', 'string', 'email', 'max:255', Rule::unique('users')->ignore($this->user)]
+        ];
+    }
+}
