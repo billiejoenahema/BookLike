0a036796d9d0bc7fe10acc5acffbbab97dda6733
billiejoenahema <billiejoenahema@gmail.com>
2021-01-05 22:15:14 +0900

    improve: 404 error message

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,3 +1,3 @@
 @extends('errors.layouts.base')
 @section('title', '404 Not Found')
-@section('message', 'お探しのページが見つかりません。URLに誤りがあるか、お探しのページは移動もしくは削除された可能性があります。')
+@section('message', 'お探しのページが見つかりません。URLに誤りがあるか、移動もしくは削除された可能性があります。')
