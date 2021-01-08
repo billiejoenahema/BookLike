eaf05739ee865895c19a3052d38429716e1970e0
billiejoenahema <billiejoenahema@gmail.com>
2021-01-08 22:17:26 +0900

    improve: style on review text, change: tool-tip to hover effect in Reviews

--- resources/js/functions/omittedText.js
+++ resources/js/functions/omittedText.js
@@ -1,7 +1,7 @@
 export default function omittedText(description, int) {
     // 文字数を制限して自己紹介文を表示
     if (description.length > int) {
-        return `${description.substr(0, int)}...`;
+        return `${description.substr(0, int)} ...続きを読む`;
     }
 
     return description;
