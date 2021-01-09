840118531f14efbf361eb707669f1ba14b897c5d
billiejoenahema <billiejoenahema@gmail.com>
2021-01-09 21:09:14 +0900

    add: toString() to formatDate.js

--- public/js/all.js
+++ public/js/all.js
@@ -154,7 +154,7 @@ function formatDate(createdAt, format) {
     format = format.replace(/yyyy/g, date.getFullYear())
     format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2))
     format = format.replace(/dd/g, ('0' + date.getDate()).slice(-2))
-    return format
+    return format.toString()
 };
 
 const scrollTop = (e) => {
