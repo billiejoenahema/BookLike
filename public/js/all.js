8d9afd8a8f11c65a1bee2e6b31058af8a214ff32
billiejoenahema <billiejoenahema@gmail.com>
2021-01-09 21:43:41 +0900

    add: replace(/-/g, '/') to Date object for safari

--- public/js/all.js
+++ public/js/all.js
@@ -150,12 +150,13 @@ window.addEventListener("scroll", () => {
 })
 
 function formatDate(createdAt, format) {
-    const date = new Date(createdAt)
+    // safariで表示させるためにreplace(/-/g,'/')で加工
+    const date = new Date(createdAt.replace(/-/g, '/'))
     format = format.replace(/yyyy/g, date.getFullYear())
     format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2))
     format = format.replace(/dd/g, ('0' + date.getDate()).slice(-2))
-    return format.toString()
-};
+    return format
+}
 
 const scrollTop = (e) => {
     'use strict'
--- public/js/formatDate.js
+++ public/js/formatDate.js
@@ -1,7 +1,8 @@
 function formatDate(createdAt, format) {
-    const date = new Date(createdAt)
+    // safariで表示させるためにreplace(/-/g,'/')で加工
+    const date = new Date(createdAt.replace(/-/g, '/'))
     format = format.replace(/yyyy/g, date.getFullYear())
     format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2))
     format = format.replace(/dd/g, ('0' + date.getDate()).slice(-2))
-    return format.toString()
-};
+    return format
+}
