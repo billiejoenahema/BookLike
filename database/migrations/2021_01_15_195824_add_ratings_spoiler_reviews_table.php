c1669807cae05329f6341acb139e0a0351e242f7
billiejoenahema <billiejoenahema@gmail.com>
2021-01-15 20:34:17 +0900

    rename: column from recommend to ratings

--- database/migrations/2021_01_15_195824_add_ratings_spoiler_reviews_table.php
+++ database/migrations/2021_01_15_195824_add_ratings_spoiler_reviews_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddRecommendSpoilerReviewsTable extends Migration
+class AddRatingsSpoilerReviewsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,8 @@ class AddRecommendSpoilerReviewsTable extends Migration
     public function up()
     {
         Schema::table('reviews', function (Blueprint $table) {
-            $table->unsignedTinyInteger('recommend')->default('3')->after('image_url')->comment('おすすめ度');
-            $table->unsignedTinyInteger('spoiler')->default('0')->after('recommend')->comment('ネタバレ');
+            $table->unsignedTinyInteger('ratings')->default('3')->after('image_url')->comment('おすすめ度');
+            $table->unsignedTinyInteger('spoiler')->default('0')->after('ratings')->comment('ネタバレ');
         });
     }
 
@@ -27,7 +27,7 @@ class AddRecommendSpoilerReviewsTable extends Migration
     public function down()
     {
         Schema::table('reviews', function (Blueprint $table) {
-            $table->dropColumn('recommend');
+            $table->dropColumn('ratings');
             $table->dropColumn('spoiler');
         });
     }
