d588c1843dd2e03523ddeb89282cd162346f4417
billiejoenahema <billiejoenahema@gmail.com>
2021-01-16 12:14:06 +0900

    remove: dead code ReactToolTip in Users component

--- resources/js/components/users/Users.js
+++ resources/js/components/users/Users.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactTooltip from 'react-tooltip'
 import FollowButton from './FollowButton'
 import FollowerCount from './FollowerCount'
 import TotalFavoritesCount from './TotalFavoritesCount'
