4dbdfa09fe66f1509b5ff314e11196f9d6e66cc6
billiejoenahema <billiejoenahema@gmail.com>
2021-01-16 20:29:05 +0900

    create: onLoad.js&changeStars.js

--- public/js/onLoad.js
+++ public/js/onLoad.js
@@ -1,3 +1,17 @@
+// 投稿編集ページにおける星評価の初期値
+const showRatings = (initialRatings) => {
+    let ratings = initialRatings
+    const starElements = document.querySelectorAll('.edit-star')
+
+    starElements.forEach((starElement, index) => {
+        // ratingの数値だけ星に色をつける
+        if (index < ratings) {
+            starElement.classList.replace('far', 'fas')
+        }
+    })
+}
+
+// スマホ用フッターメニューの現在のページのアイコンに色をつける
 function currentPageHighlight() {
     'use strict'
     const path = window.location.pathname
@@ -32,4 +46,12 @@ function currentPageHighlight() {
     }
 }
 
-window.onload = currentPageHighlight()
+window.addEventListener('load', () => {
+    //ページが読み込まれたときにratingsの初期値を取得する
+    if (document.getElementById('ratings')) {
+        const initialRatings = document.getElementById('ratings').dataset.ratings
+        initialRatings && showRatings(initialRatings)
+    }
+
+    currentPageHighlight()
+})
