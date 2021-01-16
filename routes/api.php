2aecc9156a38ba8862f955408a96b9eef3640f14
billiejoenahema <billiejoenahema@gmail.com>
2021-01-16 10:52:58 +0900

    improve: reduce code in api.php

--- routes/api.php
+++ routes/api.php
@@ -3,22 +3,20 @@
 Route::group(['middleware' => 'auth'], function() {
 
     // 書籍検索（resourceよりも上に書かないと'404 Not found'になってしまう）
-    Route::get('reviews/search_items', 'Api\SearchItemsController')->name('search_items');
+    Route::get('/reviews/search_items', 'Api\SearchItemsController')->name('search_items');
 
-    Route::get('/reviews/{review}', 'Api\ReviewController@show');
+    // 投稿関連
+    Route::resource('/reviews', 'Api\ReviewController', ['only' => ['index', 'show']]);
 
-    Route::get('/reviews', 'Api\ReviewController@index');
-
-    Route::get('/users/{user}', 'Api\UsersController@show');
-
-    Route::get('/users', 'Api\UsersController@index');
+    // ユーザー関連
+    Route::resource('/users', 'Api\UsersController', ['only' => ['index', 'show']]);
 
     // いいね機能
-    Route::post('add_favorite/{id}', 'Api\FavoriteController@addFavorite');
-    Route::post('remove_favorite/{id}', 'Api\FavoriteController@removeFavorite');
+    Route::post('/add_favorite/{id}', 'Api\FavoriteController@addFavorite');
+    Route::post('/remove_favorite/{id}', 'Api\FavoriteController@removeFavorite');
 
     // フォロー/フォロー解除
-    Route::post('follow/{id}', 'Api\FollowController@follow');
-    Route::post('unfollow/{id}', 'Api\FollowController@unfollow');
+    Route::post('/follow/{id}', 'Api\FollowController@follow');
+    Route::post('/unfollow/{id}', 'Api\FollowController@unfollow');
 
 });
