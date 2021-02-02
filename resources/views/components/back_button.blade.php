54af69268e8fa4f0af9fee18b41fb496e8b4036c
billiejoenahema <billiejoenahema@gmail.com>
2021-02-02 21:31:45 +0900

    fix: style back_button&search_modal_button

--- resources/views/components/back_button.blade.php
+++ resources/views/components/back_button.blade.php
@@ -1,3 +1,3 @@
-<div type="button" onclick="history.back()" class="d-inline my-2 text-blog internal-link">
+<div type="button" onclick="history.back()" class="d-inline my-2 text-blog">
     <i class="fas fa-chevron-left"></i> 戻る
 </div>
