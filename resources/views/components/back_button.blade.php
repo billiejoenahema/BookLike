ff1f1862a275f54602686efe4fb0cd8fcbdfd82f
billiejoenahema <billiejoenahema@gmail.com>
2021-02-02 20:43:28 +0900

    improve: back&mobile_search button, add: hover effect to favorite_button

--- resources/views/components/back_button.blade.php
+++ resources/views/components/back_button.blade.php
@@ -1,3 +1,3 @@
-<div type="button" onclick="history.back()" class="text-blog internal-link">
+<div type="button" onclick="history.back()" class="d-inline my-2 text-blog internal-link">
     <i class="fas fa-chevron-left"></i> 戻る
 </div>
