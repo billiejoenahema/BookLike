a3129931daf43cdefe217df46387c882659e3376
billiejoenahema <billiejoenahema@gmail.com>
2021-02-07 10:50:55 +0900

    fix: style margin-bottom of label

--- resources/js/components/reviews/ReviewIndex.js
+++ resources/js/components/reviews/ReviewIndex.js
@@ -221,7 +221,7 @@ const ReviewIndex = () => {
                 </select>
                 {/* 並び替え */}
                 <div className="d-flex flex-row p-0 mt-1 mt-sm-0">
-                    <label htmlFor="selectSort" className="text-right py-1 mr-1">並び替え</label>
+                    <label htmlFor="selectSort" className="text-right py-1 mr-1 mb-0">並び替え</label>
                     <select onChange={sortChange} className="form-control-sm" id="selectSort">
                         <option value="default">新着順</option>
                         <option value="favorite">いいね数</option>
