d3699e705b36750d1731c9bcef93c29085baa452
billiejoenahema <billiejoenahema@gmail.com>
2021-05-30 21:44:13 +0900

    fix: add braces to props

--- resources/js/components/users/CreatedAt.js
+++ resources/js/components/users/CreatedAt.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const CreatedAt = (createdAt) => {
+const CreatedAt = ({ createdAt }) => {
   return (
     <div className="d-flex justify-content-end flex-grow-1">
       <p className="mb-0 text-secondary">{formatDate(createdAt, 'yyyy/MM/dd')}</p>
