855d161a09c16b65e8be63b477d09c6400357819
billiejoenahema <billiejoenahema@gmail.com>
2021-10-19 19:46:55 +0900

    update: notification mail template

---
+++ resources/views/vendor/notifications/email.blade.php
@@ -0,0 +1,41 @@
+@component('mail::message')
+{{-- Greeting --}}
+@if (! empty($greeting)) # {{ $greeting }}
+@else @if ($level === 'error') # @lang('Whoops!') @else # @lang('こんにちは!')
+@endif @endif
+
+{{-- Intro Lines --}}
+@foreach ($introLines as $line)
+{{ $line }}
+
+@endforeach
+
+{{-- Action Button --}}
+@isset($actionText)
+<?php
+    switch ($level) {
+        case 'success':
+        case 'error':
+            $color = $level;
+            break;
+        default:
+            $color = 'primary';
+    }
+?>
+@component('mail::button', ['url' => $actionUrl, 'color' => $color])
+{{ $actionText }}
+@endcomponent @endisset
+
+{{-- Outro Lines --}}
+@foreach ($outroLines as $line)
+{{ $line }}
+
+@endforeach
+
+{{-- Subcopy --}}
+@isset($actionText) @slot('subcopy') @lang(
+"もし、「:actionText」ボタンがうまく機能しない場合は、以下のURLをブラウザのURL欄にコピー&ペーストしてアクセスして下さい。
+[:actionURL](:actionURL)", [ 'actionText' => $actionText, 'actionURL' =>
+$actionUrl, ] )
+<span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
+@endslot @endisset @endcomponent
