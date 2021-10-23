e39e9cf16c54fba5572cf58e4ff9145b7fd251bd
billiejoenahema <billiejoenahema@gmail.com>
2021-10-23 18:30:49 +0900

    create: ReviewPolicy

--- app/Policies/ReviewPolicy.php
+++ app/Policies/ReviewPolicy.php
@@ -3,15 +3,15 @@
 namespace App\Policies;
 
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
+use App\Models\Review;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-class UserPolicy
+class ReviewPolicy
 {
     use HandlesAuthorization;
 
     /**
-     * Determine whether the user can view any models.
+     * Determine whether the user can view any reviews.
      *
      * @param  \App\Models\User  $user
      * @return mixed
@@ -22,9 +22,10 @@ class UserPolicy
     }
 
     /**
-     * Determine whether the user can view the model.
+     * Determine whether the user can view the review.
      *
      * @param  \App\Models\User  $user
+     * @param  \App\Review  $review
      * @return mixed
      */
     public function view(User $user)
@@ -33,42 +34,45 @@ class UserPolicy
     }
 
     /**
-     * Determine whether the user can create models.
+     * Determine whether the user can create reviews.
      *
      * @param  \App\Models\User  $user
      * @return mixed
      */
-    public function create(User $user)
+    public function create(User $user, Review $review)
     {
-        //
+        return $user->id === $review->user_id;
     }
 
     /**
-     * Determine whether the user can update the model.
+     * Determine whether the user can update the review.
      *
      * @param  \App\Models\User  $user
+     * @param  \App\Review  $review
      * @return mixed
      */
-    public function update(User $user)
+    public function update(User $user, Review $review)
     {
-        return $user->id === Auth::user()->id;
+        return $user->id === $review->user_id;
     }
 
     /**
-     * Determine whether the user can delete the model.
+     * Determine whether the user can delete the review.
      *
      * @param  \App\Models\User  $user
+     * @param  \App\Review  $review
      * @return mixed
      */
-    public function delete(User $user)
+    public function delete(User $user, Review $review)
     {
-        return $user->id === Auth::user()->id;
+        return $user->id === $review->user_id;
     }
 
     /**
-     * Determine whether the user can restore the model.
+     * Determine whether the user can restore the review.
      *
      * @param  \App\Models\User  $user
+     * @param  \App\Review  $review
      * @return mixed
      */
     public function restore(User $user)
@@ -77,9 +81,10 @@ class UserPolicy
     }
 
     /**
-     * Determine whether the user can permanently delete the model.
+     * Determine whether the user can permanently delete the review.
      *
      * @param  \App\Models\User  $user
+     * @param  \App\Review  $review
      * @return mixed
      */
     public function forceDelete(User $user)
