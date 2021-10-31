488c963f1506db6f9492cc9883a9a16b0d1366d3
billiejoenahema <billiejoenahema@gmail.com>
2021-10-31 21:33:46 +0900

    create: CommentResource

---
+++ app/Http/Resources/CommentResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'review_id' => $this->review_id,
+            'text' => $this->text,
+            'user' => new UserResource($this->user),
+        ];
+    }
+}
