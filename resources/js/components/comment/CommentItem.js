bd24f5998d071c45d0707058e305e216b1240f42
billiejoenahema <billiejoenahema@gmail.com>
2021-11-02 21:11:31 +0900

    replace: comment component from blade to React

--- resources/js/components/comment/CommentItem.js
+++ resources/js/components/comment/CommentItem.js
@@ -1,50 +1,58 @@
-import React from 'react'
+import React from 'react';
+import UserIcon from '../users/UserIcon';
 
-const CommentItem = ({comment}) => {
-    return (
+const CommentItem = ({ comment, loginUser }) => {
+  return (
+    <>
+      {comment.deleted_at ? (
+        <div className="card-header p-3 border-bottom-0 bg-white d-flex flex-column">
+          <div className="d-flex flex-column text-right">
+            <span className="text-secondary">{comment.created_at}</span>
+          </div>
+          <div className="text-secondary">このコメントは削除されました</div>
+        </div>
+      ) : (
         <div>
-                {comment.deleted_at ?
-            <div className="card-header p-3 border-bottom-0 bg-white d-flex flex-column">
-                <div className="d-flex flex-column text-right">
-                    <span className="text-secondary">{ comment.created_at}</span>
-                </div>
-                <div className="text-secondary">このコメントは削除されました</div>
-            </div>
-                :
-                <div
-                className="
+          <div
+            className="
                     card-header
                     p-3
                     border-bottom-0
                     bg-white
                     d-flex
                     flex-row
-                    justify-content-between
-                ">
-                <div className="d-flex flex-row">
-                    <div className="ml-2 d-flex flex-column justify-content-between">
+                    justify-content-between"
+          >
+            <div className="d-flex flex-row">
+              <div className="ml-2 d-flex flex-column justify-content-between">
                 <UserIcon
-                    profileImage={user.profile_image}
-                    iconSize={48}
+                  profileImage={comment.user.profile_image}
+                  iconSize={48}
                 />
-                        <p className="mb-0">{comment.user.name}</p>
-                        <span
-                            className="text-secondary"
-                            >{comment.user.screen_name}</span
-                        >
-                    </div>
+                <div className="ml-2 d-flex flex-column justify-content-between">
+                  <p className="mb-0">{comment.user.name}</p>
+                  <span className="text-secondary">
+                    {comment.user.screen_name}
+                  </span>
                 </div>
-                <div className="d-flex flex-column text-right">
-                    <form>
-                            <h5><i className="fas fa-trash"></i></h5>
-                    </form>
-                    <span className="text-secondary">{comment.created_at}</span>
-                </div>
+              </div>
             </div>
-            <div className="card-body p-3">{comment.text}</div>
-    }
+            <div className="d-flex flex-column text-right">
+              {comment.user.id === loginUser.id && (
+                <form>
+                  <h5>
+                    <i className="fas fa-trash"></i>
+                  </h5>
+                </form>
+              )}
+              <span className="text-secondary">{comment.created_at}</span>
+            </div>
+          </div>
+          <div className="card-body p-3">{comment.text}</div>
         </div>
-    )
-}
+      )}
+    </>
+  );
+};
 
-export default CommentItem
+export default CommentItem;
--- resources/js/components/comment/CommentList.js
+++ resources/js/components/comment/CommentList.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import UserIcon from '../users/UserIcon';
+import CommentItem from './CommentItem';
 
-const CommentList = () => {
+const CommentList = ({ comments, loginUser }) => {
   return (
     <div>
       <h6 id="comment" className="mt-4">
         コメント
       </h6>
       <ul className="list-group">
-        {comments.map((comment) => {
-          <li className="list-group-item p-0">
-            <CommentItem comment={comment} />
-          </li>;
-        })}
+        {comments &&
+          comments.map((comment) => (
+            <li className="list-group-item p-0" key={comment.id}>
+              <CommentItem comment={comment} loginUser={loginUser} />
+            </li>
+          ))}
         <li className="list-group-item">
           <p className="mb-0 text-secondary">コメントはまだありません</p>
         </li>
