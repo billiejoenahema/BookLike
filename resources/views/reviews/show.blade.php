765e6361ac85192ab87610adba457b6cd7e3df76
billiejoenahema <billiejoenahema@gmail.com>
2021-11-02 21:06:10 +0900

    fix: blade bag

--- resources/views/reviews/show.blade.php
+++ resources/views/reviews/show.blade.php
@@ -2,7 +2,4 @@
 @include('components.back_button')
 <h6 class="mt-4">レビュー詳細</h6>
 <div id="showReview"></div>
-
-<!-- コメント -->
-@include('components.comment') @include('components.delete_review_confirm')
 @endsection
