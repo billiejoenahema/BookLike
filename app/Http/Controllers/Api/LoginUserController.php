fda314345166aa208c8269521df1176dfe2bf452
billiejoenahema <billiejoenahema@gmail.com>
2021-11-07 19:36:51 +0900

    improve: change LoginUserController to single action

--- app/Http/Controllers/Api/LoginUserController.php
+++ app/Http/Controllers/Api/LoginUserController.php
@@ -5,54 +5,18 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Http\Resources\LoginUserResource;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
 
 class LoginUserController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * ログインユーザーの情報を返します。
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function __invoke()
     {
         $login_user = User::with(['follows:id', 'followers:id'])->findOrFail(Auth::id());
         return new LoginUserResource($login_user);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, User $user)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(User $user)
-    {
-        //
     }
 }
