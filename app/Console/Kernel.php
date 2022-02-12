406be4810770aab0d931473d0038dd0ca94071bc
billiejoenahema <billiejoenahema@gmail.com>
2022-02-12 18:44:43 +0900

    fix: add BatchTest namespace to Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
+use App\Console\Commands\BatchTest;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
